321d126720d04f94fc8ea98dfc2992cf914b066a
Felix Staud <felix.staud@headwire.com>
2024-01-29 10:48:19 -0600

    start setting up filter dialog

--- blocks/inventory-browser/inventory-browser.js
+++ blocks/inventory-browser/inventory-browser.js
@@ -91,6 +91,67 @@ async function loadCars(href) {
 }
 
 /**
+ * @param {Car[]} cars
+ */
+function createFiltersElement(cars) {
+  const wrapperEl = document.createElement('div');
+  wrapperEl.classList.add('filters-wrapper');
+
+  const innerHTML = `
+    <button id="open-filter-dialog-btn" class="secondary">${createIconElement('filters').outerHTML} Filter / Sort</button>
+    
+    <div id="filter-dialog" class="hidden">
+      <div class="dialog-content">
+        <button id="close-filter-dialog-btn" class="secondary">Done</button>
+        <h1>CSS + HTML only Accordion Element</h1>
+        <ul>
+          <li>
+            <input type="checkbox" checked>
+            <i></i>
+            <h2>Languages Used</h2>
+            <p>This page was written in HTML and CSS. The CSS was compiled from SASS. I used Normalize as my CSS reset and -prefix-free to save myself some headaches. I haven't quite gotten the hang of Slim for compiling into HTML, but someday I'll use it since its syntax compliments that of SASS. Regardless, this could all be done in plain HTML and CSS.</p>
+          </li>
+          <li>
+            <input type="checkbox" checked>
+            <i></i>
+            <h2>How it Works</h2>
+            <p>Using the sibling and checked selectors, we can determine the styling of sibling elements based on the checked state of the checkbox input element. One use, as demonstrated here, is an entirely CSS and HTML accordion element. Media queries are used to make the element responsive to different screen sizes.</p>
+          </li>
+          <li>
+            <input type="checkbox" checked>
+            <i></i>
+            <h2>Points of Interest</h2>
+            <p>By making the open state default for when :checked isn't detected, we can make this system accessable for browsers that don't recognize :checked. The fallback is simply an open accordion. The accordion can be manipulated with Javascript (if needed) by changing the "checked" property of the input element.</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  `;
+
+  wrapperEl.innerHTML = innerHTML;
+  /** @type {HTMLButtonElement} */
+  const openFilterDialogBtn = wrapperEl.querySelector('#open-filter-dialog-btn');
+  /** @type {HTMLDivElement} */
+  const filterDialog = wrapperEl.querySelector('#filter-dialog');
+  /** @type {HTMLButtonElement} */
+  const closeFilterDialogBtn = wrapperEl.querySelector('#close-filter-dialog-btn');
+
+  openFilterDialogBtn.addEventListener('click', () => {
+    filterDialog.classList.remove('hidden');
+    document.body.classList.add('dialog-open');
+  });
+
+  closeFilterDialogBtn.addEventListener('click', () => {
+    filterDialog.classList.add('hidden');
+    document.body.classList.remove('dialog-open');
+  });
+
+  decorateIcons(wrapperEl);
+
+  return wrapperEl;
+}
+
+/**
  * creates an car image element
  * @param {Car} car
  * @returns {HTMLDivElement}
@@ -301,8 +362,10 @@ export default async function decorate(block) {
   inventoryDiv.classList.add('inventory');
 
   const searchEl = createSearchElement();
+  const filterEl = createFiltersElement(state.cars);
 
   inventoryDiv.appendChild(searchEl);
+  inventoryDiv.appendChild(filterEl);
   inventoryDiv.appendChild(state.inventoryUl);
 
   renderCarElement(state.cars);
