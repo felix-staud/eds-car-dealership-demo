94a0193782d6373413ec38b47ca6807a300925f1
Felix Staud <felix.staud@headwire.com>
2024-01-31 13:28:08 -0600

    use aem.min.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,4 +1,4 @@
-import { fetchPlaceholders } from '../../scripts/aem.js';
+import { fetchPlaceholders } from '../../scripts/aem.min.js';
 
 function updateActiveSlide(slide) {
   const block = slide.closest('.carousel');
