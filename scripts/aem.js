488301cb4c77483c5b95d82a61812368ea5ddbf5
Felix Staud <felix.staud@headwire.com>
2024-02-05 10:25:17 -0600

    update aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-import { getWindowSafe } from "./utils";
+import { getWindowSafe } from './utils.js';
 
 /* eslint-env browser */
 
